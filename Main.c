944bc2a9515518a088fd063b36c26a93073472ea
Verticalfile30 <stopmotion45b@gmail.com>
2019-08-18 21:37:52 -0400

    fix bugs

--- Main.c
+++ Main.c
@@ -6,7 +6,7 @@ int main() {
 	double GPA = 3.4;
 	int gradeNumber = 97;
 
-	printf("My grade is %d. \n My GPA is %d. \n The letter grade is an %s. \n");
+	printf("My grade is %d. \n My GPA is %d. \n The letter grade is an %s. \n", gradeNumber, GPA, grade);
 	
 	return 0;
 
